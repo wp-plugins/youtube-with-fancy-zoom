faf70d0a7e44e18806ebe07dfa6f5403697f658f
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-31 06:06:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/youtube-with-fancy-zoom/tags/4.0@269689 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ extra.php
@@ -0,0 +1,24 @@
+<?php
+// This page is to check is the user really using this plugin or they just download to see the code, 
+// You can remove this ad easily
+// To remove simply delete this file and go to mainpage and remove the line include_once("extra.php");
+function gads() 
+{
+	?>
+	<div align="center">
+	<script type="text/javascript"><!--
+	google_ad_client = "pub-6676019656533652";
+	/* 250x250, created 10/10/09 */
+	google_ad_slot = "3944096862";
+	google_ad_width = 250;
+	google_ad_height = 250;
+	//-->
+	</script>
+	<script type="text/javascript"
+	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+	</script>	
+	</div>
+    <?php
+	return timepass;
+}
+?>
